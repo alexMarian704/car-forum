c4019fb66ff9163f0889a42bbc6824e440c7cf6a
Alex Danciu <alex.boos.123441@gmail.com>
2021-03-30 14:47:49 +0300

    delete post

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase'
-import { Redirect, useParams } from 'react-router';
+import { Redirect, useParams , useHistory } from 'react-router';
 import { compose } from 'redux'
 import { motion } from 'framer-motion'
 import { createComment } from '../store/actions/commentAction';
+import { app } from '../config/base'
 
-function SingleProject({ project, auth , createComment , comment}) {
-    const [commentA, setComment] = useState({parentPost:'', commentText:''});
-    const [inputValue , setInputValue] = useState(null)
+function SingleProject({ project, auth, createComment, comment }) {
+    const [commentA, setComment] = useState({ parentPost: '', commentText: '' });
+    const [inputValue, setInputValue] = useState('')
     const { id } = useParams();
+    const db = app.firestore()
+    const history = useHistory();
+    const [isDeleted , setisDeleted] = useState(false)
 
     if (!auth.uid)
         return (
@@ -19,14 +23,19 @@ function SingleProject({ project, auth , createComment , comment}) {
         e.preventDefault()
     }
     const changeComment = (e) => {
-        setComment({...commentA , parentPost: id, commentText:e.target.value})
+        setComment({ ...commentA, parentPost: id, commentText: e.target.value })
     }
     const postComment = () => {
         createComment(commentA)
         setInputValue('')
     }
+    const deletePost =  async (e) => {
+        setisDeleted(true);
+        await db.collection('projects').doc(id).delete()
+        history.push('/');
+    }
 
-    if (project) {
+    if (project && isDeleted===false) {
         const singleProject = project[0];
         return (
             <motion.div
@@ -41,13 +50,16 @@ function SingleProject({ project, auth , createComment , comment}) {
                         <p>{singleProject.content}</p>
                         <h2>Posted by {singleProject.authorFirstName} {singleProject.authorLastName}</h2>
                         <h3>{singleProject.createdAt.toDate().toDateString()}</h3>
-                        {singleProject.file && <img src={singleProject.file} alt={singleProject.title}/>}
+                        {singleProject.file && <img src={singleProject.file} alt={singleProject.title} />}
+                        <br />
+                        { (auth.uid === singleProject.authorId ||
+                        auth.uid === `fF1LRlXcvrSHgycDhH5XQFzetFo1`) &&<button id="createBut" onClick={deletePost}>Delete</button>}
                     </div>
                     <div>
                         <h2>Comments:</h2>
-                        {comment &&comment.map((comm , index)=>{
-                            if(comm.parentPost === id){
-                                return(
+                        {comment && comment.map((comm, index) => {
+                            if (comm.parentPost === id) {
+                                return (
                                     <div key={index} id="commentContainer">
                                         <h2>{comm.authorFirstName} {comm.authorLasttName}</h2>
                                         <p>{comm.commentText}</p>
@@ -58,11 +70,11 @@ function SingleProject({ project, auth , createComment , comment}) {
                         })}
                     </div>
                     <br />
-                    <hr/>
+                    <hr />
                     <div>
                         <form onSubmit={handleSubmit}>
                             <label >Add comment</label>
-                            <input type="text" id="comment" onChange={changeComment} autoComplete="off" value={inputValue} required/>
+                            <input type="text" id="comment" onChange={changeComment} autoComplete="off" value={inputValue} required />
                             <br />
                             <button onClick={postComment} id="createBut">Post</button>
                         </form>
@@ -70,7 +82,7 @@ function SingleProject({ project, auth , createComment , comment}) {
                 </div>
             </motion.div>
         )
-    } else {
+    }else {
         return (
             <h1 id="isLoading">Loading</h1>
         )
@@ -94,11 +106,11 @@ const mapDispatchProps = (dispatch) => {
 }
 
 export default compose(
-    connect(mapStateToProps , mapDispatchProps),
+    connect(mapStateToProps, mapDispatchProps),
     firestoreConnect(props => [
         { collection: 'projects', doc: props.match.params.id },
-        { collection: 'comments' , orderBy: ['createdAt' , 'desc']}
-        
+        { collection: 'comments', orderBy: ['createdAt', 'desc'] }
+
     ])
 )(SingleProject)
 
